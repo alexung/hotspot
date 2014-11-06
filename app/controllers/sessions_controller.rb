f1cb48def62e04211796b8c5b21497a95a0d7256
kkane106 <kkane106@gmail.com>
2014-11-06 13:21:56 -0500

    Add session create in SessionController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+	def create
+		if user = User.find(params[session_params])
+			session[:user_id] = user.id
+			flash[:success]
+			redirect_to user_path(user)
+		else
+			flash[:error]
+			redirect_to root_path
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_path
+	end
+
 end
