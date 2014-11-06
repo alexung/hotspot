f3fccf0dc13173f022f232f88831951a070660b1
steveono <steveono@gmail.com>
2014-11-06 12:00:13 -0500

    started oauth2 login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,10 @@ module ApplicationHelper
 
   def client
     client ||= OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET, {
-              :site => 'https://github.com/login/oauth/authorize',
-              :authorize_url => "/o/oauth2/auth",
+              # :site => 'https://github.com/login/oauth/authorize',
+              :client_id => CLIENT_ID,
+              :client_secret => CLIENT_SECRET,
+              :authorize_url => "https://github.com/login/oauth/authorize",
               :token_url => "/o/oauth2/token"
             })
   end
