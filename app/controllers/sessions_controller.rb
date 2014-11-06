eb9fbb8247d233dba8bb55f0fece5dc7bece3a72
kkane106 <kkane106@gmail.com>
2014-11-06 18:54:08 -0500

    Remove deprecated session method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,5 @@
 class SessionsController < ApplicationController
 
-	def self.from_omniauth(auth)
-		auth_email = auth[:info][:email]
-
-		self.where(email: auth_email).first || create_with_omniauth(auth)
-	end
-
 	def create
 		auth = env['omniauth.auth']
 		user = User.find_or_create_by(email: auth[:info][:email].first)
