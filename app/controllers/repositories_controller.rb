067f503941a0e6ea370b03634ba9699cacd5677d
kkane106 <kkane106@gmail.com>
2014-11-06 12:03:17 -0500

    Add repository new and repository create routes

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,4 +8,19 @@ class RepositoriesController < ApplicationController
     @repository = Repository.find(params[:id])
   end
 
+  def new
+  	@repository = Repository.new
+  end
+
+  def create
+  	@repository = Repository.new(repository_params)
+  	if @repository.save
+  		flash[:success]
+  		redirect_to @repository
+  	else
+  		flash[:error]
+  		redirect_to 'new'
+  	end
+  end
+
 end
