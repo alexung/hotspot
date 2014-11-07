03f0f6648cdb48b43cf6b4bd57467e1f02e93584
Alex Ung <alexung.ucsb@gmail.com>
2014-11-07 11:04:22 -0500

    Search bar works for users

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,15 +2,15 @@ class RepositoriesController < ApplicationController
 require 'json'
 
   def index
-    # @user = params[:keyword]
-    # repos = `curl https://api.github.com/users/#{user_name}/repos`
-    # @repositories = JSON.parse(repos)
+    @user = params[:keyword]
+    @repos = `curl https://api.github.com/users/#{@user}/repos?client_id=891ce1a4c1d52b232e3d&f216b8fad1efd3cddc36f8fe7831c7aadbb9e4fd`
+    @repositories = JSON.parse(@repos)
   end
 
   def show
     repo_name = params["name"]
     owner = current_user.username
-    repo_commits = `curl https://api.github.com/repos/#{owner}/#{repo_name}/commits`
+    repo_commits = `curl https://api.github.com/repos/#{owner}/#{repo_name}/commits?client_id=#{ENV['GITHUB_KEY']}&#{ENV['GITHUB_SECRET']}`
     @commits = JSON.parse(repo_commits)
   end
 
@@ -31,12 +31,10 @@ require 'json'
 
   def search
     @user = params[:keyword]
-    @repos = `curl -i https://api.github.com/users/"#{@user}"/repos`
-    #redirect_to '/'
-    #binding.pry
-    #@repositories = JSON.parse(@repos)
-    #binding.pry
-    redirect_to repositories_path
+    # @repos = `curl -i https://api.github.com/users/"#{@user}"/repos`
+    # redirect_to '/'
+    # @repositories = JSON.parse(@repos)
+    redirect_to :controller => 'repositories', action: 'index', keyword: params[:keyword]
   end
 
   private
