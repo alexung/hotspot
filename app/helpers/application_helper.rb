a3b62c4f091dd8b8ad8d11fb6ae49cede06ee9d8
Alex Ung <alexung.ucsb@gmail.com>
2014-11-07 12:11:09 -0500

    Begin skeleton for getting commits in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,24 @@
 module ApplicationHelper
+  def get_commits(user, repo)
+    @commits = `curl https://api.github.com/repos/#{user}/#{repo}/commits?client_id=#{ENV['GITHUB_KEY']}&client_secret=#{ENV['GITHUB_SECRET']}`
+    @json = Json.parse(@commits)
+  end
 
+  def get_sha
+    sha
+  end
+
+  def get_files
+  end
+
+  def files
+  end
+
+  def get_info
+  end
+
+  def add_info
+  end
 
 
 end
