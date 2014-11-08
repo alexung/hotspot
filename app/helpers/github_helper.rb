8899e43453a1b5ac726012302bc63e20191c6249
kkane106 <kkane106@gmail.com>
2014-11-08 15:32:22 -0500

    Refactor GithubHelper: -Add constants to handle base URI and API Keys -Refactor fetch_gh as base method for additional helper, includes URI and API Constant and calls curl command -Add method to fetch user repos -Add method to fetch contributors to a repo -Add method to fetch contributor avatars for use in the view

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -1,11 +1,27 @@
 module GithubHelper
   require 'json'
 
-  def fetch_gh_repos(opts={})
-    base_uri = "https://api.github.com"
-    JSON.parse(
-      `curl #{base_uri}/users/#{opts[:user]}/repos?client_id=#{ENV['GITHUB_KEY']}&client_secret=#{ENV['GITHUB_SECRET']}`
-    )
+  BASE_URI = "https://api.github.com"
+  CREDENTIALS = "?client_id=#{ENV['GITHUB_KEY']}&client_secret=#{ENV['GITHUB_SECRET']}"
+
+ 	def fetch_gh(path)
+ 		JSON.parse (`curl #{BASE_URI}/#{path}#{CREDENTIALS}`)
+ 	end
+
+  def fetch_gh_repos(user)
+    path = "users/#{user}/repos"
+    fetch_gh(path)
+  end
+
+  def fetch_gh_contributors(user, repo)
+  	path = "repos/#{user}/#{repo}/contributors"
+  	fetch_gh(path)
+  end
+
+  def fetch_contributor_avatar
+  	self.map do |contributor|
+  		contributor["avatar_url"]
+  	end
   end
 
 end
