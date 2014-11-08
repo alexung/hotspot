c20e30eaa62b22f06a8da1c5b5385bdcfacd271e
Alex Ung <alexung.ucsb@gmail.com>
2014-11-08 17:13:00 -0500

    gemfile merge conflict

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,10 +1,20 @@
 class RepositoriesController < ApplicationController
-  require 'json'
-
+  include GithubHelper
   def index
     @user = params[:username]
-    @repos = `curl https://api.github.com/users/#{@user}/repos?client_id=#{ENV['GITHUB_KEY']}&client_secret=#{ENV['GITHUB_SECRET']}`
-    @repositories = JSON.parse(@repos)
+
+    @repositories = fetch_gh_repos({:user => @user})
+
+    p "*"*50
+    p @repositories["message"]
+    p "*"*50
+
+    if @repositories["message"] != "Not Found"
+      render :index
+    rescue
+      render :error
+    end
+
   end
 
   def show
