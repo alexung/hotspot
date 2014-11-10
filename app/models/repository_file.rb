e8c8ae50f1589d925b5e501ff236a2ae125e296d
kkane106 <kkane106@gmail.com>
2014-11-10 00:03:50 -0500

    Add create_repo_files method to skinny the repository controller

--- app/models/repository_file.rb
+++ app/models/repository_file.rb
@@ -1,3 +1,16 @@
 class RepositoryFile < ActiveRecord::Base
-  belongs_to :repository
+	belongs_to :repository
+	has_many :contributers
+
+	def self.create_repo_files(path, username, repository)
+		create do |file|
+			file.github_url = "http://github.com/#{username}/#{repository.name}/blob/master/#{path[:file_path]}",
+			file.repository_id = repository.id,
+			file.name = repository.name,
+			file.commits = path[:commits],
+			file.contributers = path[:contributers],
+			file.insertions = path[:insertions],
+			file.deletions = path[:deletions]
+		end
+	end
 end
