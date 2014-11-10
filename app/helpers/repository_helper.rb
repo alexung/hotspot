d40519d58ef721bae91b0b97581afbabf2719422
kkane106 <kkane106@gmail.com>
2014-11-10 14:44:44 -0500

    Move save repository method to the repo class

--- app/helpers/repository_helper.rb
+++ app/helpers/repository_helper.rb
@@ -7,10 +7,4 @@ module RepositoryHelper
   def repository_exists?(repo_uid)
   	Repository.find_by(repo_uid: repo_uid)
   end
-
-  def save_repository_to_db(username, repository, repo_uid)
-  	repository_to_database = Repository.new(user_id: session[:user_id], name: params[:repo], url: "http://www.github.com/#{username}/#{repository}", repo_owner: username, repo_uid: repo_uid)
-    binding.pry
-    repository_to_database.save
-  end
 end
