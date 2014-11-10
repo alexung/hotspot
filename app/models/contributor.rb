ec871fb46eb5e5110a1399f22fd983e127ecac6d
kkane106 <kkane106@gmail.com>
2014-11-10 00:19:53 -0500

    Remove contributor migration and model, handling this with a .join instead.

--- app/models/contributor.rb
+++
@@ -1,3 +0,0 @@
-class Contributor < ActiveRecord::Base
-	belongs_to :repository_file
-end
--- db/migrate/20141110050141_create_contributors.rb
+++
@@ -1,8 +0,0 @@
-class CreateContributors < ActiveRecord::Migration
-  def change
-    create_table :contributors do |t|
-    	t.belongs_to :repository_file
-      t.string :avatar_path
-    end
-  end
-end
