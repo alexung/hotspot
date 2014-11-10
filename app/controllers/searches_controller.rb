3def1e6d3920bc9ca054f049f655d7a592e5d933
kkane106 <kkane106@gmail.com>
2014-11-10 17:05:38 -0500

    Render repo index

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,7 +9,7 @@ class SearchesController < ApplicationController
 		@repositories = fetch_gh_repos(@user)
 
 		if @repositories.class == Array
-			render :index
+			render 'repositories/index'
 		else
 			flash[:error] = "No user was found with that username."
 			redirect_to user_path(User.find(session[:user_id]))
