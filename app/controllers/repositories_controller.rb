1ea9b08541a179d7118a7bbb2786decd8ae92693
kkane106 <kkane106@gmail.com>
2014-11-10 00:03:16 -0500

    Begin Refactor of repo controller, move repository_files create method into repo files model

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -19,7 +19,6 @@ class RepositoriesController < ApplicationController
   end
 
   def show
-    #@saved_repositories_banner = false
     @individual_repo_banner = true
     @notes = Note.all
     @repository = params[:repo]
@@ -31,33 +30,23 @@ class RepositoriesController < ApplicationController
     if repository_exists?(@repository, @username)
       repository = Repository.find_by(repo_uid: @repo_uid)
       @repo_uid = repository.repo_uid #this keeps track of the repo_uid for later use when we recreate the repository below.
-      # binding.pry
       repository.destroy
     end
 
-    #success! saving repo to database
     @repository_to_database = Repository.new(user_id: session[:user_id], name: params[:repo], url: "http://www.github.com/#{@username}/#{@repository}", repo_owner: @username, repo_uid: @repo_uid)
     @repository_to_database.save
 
     delete_repo(@repository)
+p "*"*1000
+    @rows = CodeReview.new(@repository, @username).rows
+    p @rows
+p "*"*1000
 
-    #success! Saving repofiles to database
-    @rows_to_parse = CodeReview.new(@repository, @username)
-    # @rows_to_parse.rows
-    # binding.pry
-    @rows_to_parse.rows.map do |path|
-      RepositoryFile.create(
-                            github_url: "http://github.com/#{@username}/#{@repository}/blob/master/#{path[:file_path]}",
-                            repository_id: @repository_to_database.id,
-                            name: @repository_to_database.name,
-                            commits: path[:commits],
-                            contributers: path[:contributers].to_s,
-                            insertions: path[:insertions],
-                            deletions: path[:deletions]
-                            )
+    @rows.map do |path|
+      RepositoryFile.create_repo_files(path, @username, @repository_to_database)
     end
 
-    @rows = CodeReview.new(@repository, @username).rows.sort_by{|row_arr| -row_arr[:commits]}
+    @rows.sort_by{|row_arr| -row_arr[:commits]}
   end
 
   def change_branch
