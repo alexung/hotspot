030d8cbcf5182d18d6d7f0d4d4a427eb62ad8eaa
kkane106 <kkane106@gmail.com>
2014-11-10 09:50:59 -0500

    Skinny the show controller, start making an update route so that we are not always dropping our database files

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -24,30 +24,27 @@ class RepositoriesController < ApplicationController
     @repository = params[:repo]
     @branches = list_branches(@repository)
     @username = params[:username]
-    repo = JSON.parse(`curl https://api.github.com/repos/#{@username}/#{@repository}`)
-    @repo_uid = repo["id"]
+    repo_uid = fetch_repo_uid(username, repository)
 
-    if repository_exists?(@repo_uid)
-      repository = Repository.find_by(repo_uid: @repo_uid)
-      repository.destroy
-    end
-
-
-    delete_repo(@repository)
     @rows = CodeReview.new(@repository, @username).rows
 
-    repository_to_database = Repository.new(user_id: session[:user_id], name: params[:repo], url: "http://www.github.com/#{@username}/#{@repository}", repo_owner: @username, repo_uid: @repo_uid)
-    repository_to_database.save
+    save_repository_to_db(@username, @repository, repo_uid)
     
     @rows.map do |path|
       RepositoryFile.create_repo_files(path, @username, repository_to_database)
     end
-
     @rows.sort_by{|row_arr| -row_arr[:commits]}
   end
 
   def update
+    if repository_exists?(repo_uid)
+      repository = Repository.find_by(repo_uid: repo_uid)
+      repository.destroy
+    end
+    
+  end
 
+  def destroy
   end
 
   def change_branch
