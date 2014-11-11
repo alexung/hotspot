ce3885d1865ffea14dcc8fad2cbfae00e2594139
steveono <steveono@gmail.com>
2014-11-11 15:11:35 -0500

    added destroy button and removed old show method

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -14,54 +14,6 @@ class RepositoriesController < ApplicationController
     # @branches = list_branches(@repository.name)
   end
 
-
-# -----------------OLD SHOW METHOD--------------------------------------------
-
-
-
-  # def show
-  #   @repository = params[:repo]
-  #   @branches = list_branches(@repository)
-  #   @username = params[:username]
-  #   repo = JSON.parse(`curl https://api.github.com/repos/#{@username}/#{@repository}`)
-  #   @repo_uid = repo["id"]
-
-  #   if repository_exists?(@repository, @username)
-  #     repository = Repository.find_by(repo_uid: @repo_uid)
-  #     @repo_uid = repository.repo_uid #this keeps track of the repo_uid for later use when we recreate the repository below.
-  #     # binding.pry
-  #     repository.destroy
-  #   end
-
-  #   #success! saving repo to database
-  #   @repository_to_database = Repository.new(user_id: 1, name: params[:repo], url: "http://www.github.com/#{@username}/#{@repository}", repo_owner: @username, repo_uid: @repo_uid)
-  #   @repository_to_database.save
-
-  #   delete_repo(@repository)
-
-  #   #success! Saving repofiles to database
-  #   @rows_to_parse = CodeReview.new(@repository, @username)
-  #   # @rows_to_parse.rows
-  #   # binding.pry
-  #   @rows_to_parse.rows.map do |path|
-  #     RepositoryFile.create(
-  #                           github_url: "http://github.com/#{@username}/#{@repository}/blob/master/#{path[:file_path]}",
-  #                           repository_id: @repository_to_database.id,
-  #                           name: @repository_to_database.name,
-  #                           commits: path[:commits],
-  #                           contributers: path[:contributers].to_s,
-  #                           insertions: path[:insertions],
-  #                           deletions: path[:deletions]
-  #                           )
-  #   end
-
-  #   @rows = CodeReview.new(@repository, @username).rows.sort_by{|row_arr| -row_arr[:commits]}
-  # end
-
-
-# -----------------OLD SHOW METHOD--------------------------------------------
-
-
   def update
     repository = Repository.find_by(name: params[:repository], owner_name: params[:username])
     rows = CodeReview.new(params[:repository], params[:username]).rows
