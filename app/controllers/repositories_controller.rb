3ee1c9954d07a62b6a444b0268021fb92e550958
kkane106 <kkane106@gmail.com>
2014-11-10 19:31:40 -0500

    Small update to spacing on update route

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,7 +10,6 @@ class RepositoriesController < ApplicationController
   end
 
   def update
-      # repository = Repository.find_by(repo_uid: repo_uid)
     username = params[:username]
     repository = Repository.find_by(name: params[:repository], owner_name: username)
     @rows = CodeReview.new(repository, username).rows
@@ -20,7 +19,6 @@ class RepositoriesController < ApplicationController
         file.update(params)
       else
         repository.repository_files.create(params)
-          # FIGURE OUT DELETE of old/deleted files from repo
       end
     end
   end
@@ -37,5 +35,5 @@ class RepositoriesController < ApplicationController
 
   def repository_params
    params.require(:repository).permit()
-  end
+ end
 end
