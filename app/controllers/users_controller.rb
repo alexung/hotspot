228a00a7b6e825072dc8374dc7a8348647f0a405
Alex Ung <alexung.ucsb@gmail.com>
2014-11-11 12:54:27 -0500

    Delete button works from user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
+  include ApplicationHelper
 
   def show
     @user = User.find(session[:user_id])
     @repositories = @user.repositories
+  end
+
+   def destroy
+    @repositories = User.find(params["id"]).repositories
+    delete_repo(repository.name)
+    repository.destroy
+    #@repository = Repository.find(params[:id])
+    # delete from /tmp
+    #delete_repo(@repository.name)
+    #@repository.destroy
+    redirect_to user_path(User.find(session[:user_id]))
   end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 	Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :destroy]
   resources :repositories, except: [:edit] do
     resources :notes
   end
