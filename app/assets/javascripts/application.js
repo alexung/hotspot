4d3b0a3fa033397eff3bcc0612d73e81b6a46f29
steveono <steveono@gmail.com>
2014-11-10 19:43:56 -0500

    basic working note partial animation, comes in from left

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,14 @@
-$( document ).ready(function() {
-  $(".sidebar_toggle").click(function() {
-    $('.sidebar').slideToggle("activated")
-  })
-});
+// $( document ).ready(function() {
+//   $(".sidebar_toggle").click(function() {
+//     $('.sidebar').slideToggle("activated")
+//   })
+// });
+
+
+
+// $( document ).ready(function() {
+//   $( "button" ).click(function() {
+//     $( ".sidebar" ).toggle( "slide" );
+//   });
+// });
+
