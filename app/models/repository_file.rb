08d07d831a0f3ca90490728a8c551e8796ba809d
kkane106 <kkane106@gmail.com>
2014-11-10 19:12:10 -0500

    Add validation to prevent duplicate paths being created

--- app/models/repository_file.rb
+++ app/models/repository_file.rb
@@ -1,6 +1,9 @@
 class RepositoryFile < ActiveRecord::Base
 	belongs_to :repository
 
+	 validates :name, uniqueness: { scope: :repository,
+    message: "File paths within a repository should be unique" }
+
 	def self.create_repo_files(path, username, repository)
 		create(
 			github_url: "http://github.com/#{username}/#{repository}/blob/master/#{path[:file_path]}",
