597ee4755cba5388330dc371de775e5c83b245d1
kkane106 <kkane106@gmail.com>
2014-11-11 17:36:06 -0500

    remove branch references in repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -6,14 +6,13 @@ class Repository < ActiveRecord::Base
 	validates :name, uniqueness: true
 	validates :url, uniqueness: true
 
-	def self.save_repository_to_db(username, repository, repo_uid, current_user, branches)
+	def self.save_repository_to_db(username, repository, repo_uid, current_user)
 		self.find_or_create_by(
 			user_id: 		current_user,
 			name: 			repository,
 			url: 				"http://www.github.com/#{username}/#{repository}",
 			repo_owner: username,
-			repo_uid: 	repo_uid,
-			branches: 	branches.join(",")
+			repo_uid: 	repo_uid
 			)
 	end
 end
