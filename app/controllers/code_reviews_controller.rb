bcccb76352be0455c539e773f9877cc4f3e5999e
kkane106 <kkane106@gmail.com>
2014-11-11 20:40:02 -0500

    Update repository controller to create contributor objects associated with repo and then build associations between them and individual files

--- app/controllers/code_reviews_controller.rb
+++ app/controllers/code_reviews_controller.rb
@@ -11,9 +11,21 @@ class CodeReviewsController < ApplicationController
 		repo_uid = params[:uid]
 		rows = CodeReview.new(@repository, @username).rows
 
+		contributors = fetch_gh_contributors(@username, @repository)
 		saved_repository = Repository.save_repository_to_db(@username, @repository, repo_uid, session[:user_id])
+		
+		contributors.map do |contributor|
+			saved_repository.contributors.create(username: contributor["login"], avatar: contributor["avatar_url"])
+		end.each do |contributor|
+			contributor.update(email: fetch_contributor_email(contributor.username))
+		end
+
 		rows.map do |repo_file|
-			RepositoryFile.create_repo_files(repo_file, @username, saved_repository)
+			new_file =	RepositoryFile.create_repo_files(repo_file, @username, saved_repository)
+			repo_file[:contributors].each do |email|
+				binding.pry
+				new_file.contributors << Contributor.find_by(email: email)
+			end
 		end
 		@repository = saved_repository
 		render 'repositories/show'
