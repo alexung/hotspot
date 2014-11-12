09c679e7a02b3feb07a2f3fcd0a018dba1f9be12
kkane106 <kkane106@gmail.com>
2014-11-12 11:29:55 -0500

    Rework controller to remove reference to the active record association to repository

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,8 +13,8 @@ class NotesController < ApplicationController
     @repository = params["note"]["repository_name"]
     @repo = Repository.find_by(name: params["note"]["repository_name"])
     @note = Note.new(notes_params)
+    @note.repo_uid = @repo.repo_uid
     @note.user_id = session[:user_id]
-    @note.repository_id = @repo.id
     @note.save
     redirect_to repository_path(Repository.find(@repo.id))
   end
@@ -22,7 +22,7 @@ class NotesController < ApplicationController
   private
 
   def notes_params
-    params.require(:note).permit(:content, :user_id, :repository_id)
+    params.require(:note).permit(:content, :user_id, :repo_uid)
   end
 
 end
