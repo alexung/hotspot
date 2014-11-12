e37abce84cae53b2b008cb1e2f6f27e065e05e0d
kkane106 <kkane106@gmail.com>
2014-11-12 00:44:50 -0500

    remove and add methods to assist with new contributer methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,5 @@
   def delete_repo(repo_name)
     `cd /tmp && rm -rf #{repo_name}`
   end
-  
-  def avatar_url(gh_email)
-    default_url = "http://www.gravatar.com/avatar/00000000000000000000000000000000?s=30"
-    gravatar_id = Digest::MD5.hexdigest(gh_email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=30&d=#{CGI.escape(default_url)}"
-  end
+
 end
