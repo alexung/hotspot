1d5b0a0d55714ca17217d6d8d85b6dddd82042f2
Alex Ung <alexung.ucsb@gmail.com>
2014-11-11 19:55:31 -0500

    Can update existing repo_files, having trouble with creating new ones

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -15,14 +15,16 @@ class RepositoriesController < ApplicationController
 
   def update
     repository = Repository.find(params["id"])
-    rows = CodeReview.new(repository.name, User.find(repository.user_id).name).rows
+    # breaking directly below this line
+    rows = CodeReview.new(repository.name, User.find(repository.user_id).github_username).rows
     rows.each do |repo_file|
-      if file = repository.repository_files.find_by(name: repo_file.name)
-        file.update(repo_file)
+      if file = repository.repository_files.find_by(name: repo_file[:file_path])
+        file.update(name: repo_file[:file_path], commits: repo_file[:commits], insertions: repo_file[:insertions], deletions: repo_file[:deletions], contributors: repo_file[:contributors].to_s )
       else
-        repository.repository_files.create(repo_file)
+        repository.repository_files.create(name: repo_file[:file_path], commits: repo_file[:commits], insertions: repo_file[:insertions], deletions: repo_file[:deletions], contributors: repo_file[:contributors].to_s)
       end
     end
+    redirect_to repository_path(repository)
   end
 
   def destroy
