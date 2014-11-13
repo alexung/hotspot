631935e872f77936916c84f584bccd01b48db228
Alex Ung <alexung.ucsb@gmail.com>
2014-11-12 21:29:49 -0500

    Sort works

--- app/controllers/code_reviews_controller.rb
+++ app/controllers/code_reviews_controller.rb
@@ -1,6 +1,7 @@
 class CodeReviewsController < ApplicationController
-
+	include ApplicationHelper
 	include CodeReviewHelper
+	helper_method :sort_column, :sort_direction
 
 	def new_code_review
 		@notes_banner = true
@@ -13,6 +14,8 @@ class CodeReviewsController < ApplicationController
 		review = CodeReview.new(@repository, @username)
 		rows = review.rows
 		saved_repository = Repository.save_repository_to_db(@username, @repository, repo_uid, session[:user_id])
+		@repository_order = saved_repository.repository_files.order(sort_column + " " + sort_direction)
+
 		contributors = saved_repository.contributors.create(create_contributors_hash(saved_repository.name))
 
 		gh_contributors = fetch_gh_contributors(@username, @repository)
@@ -21,12 +24,12 @@ class CodeReviewsController < ApplicationController
 			if gh_contributors.select {|gh_contributor| fetch_contributor_email(gh_contributor["login"]) }.include?(contributor.email)
 				gh_info = gh_contributors.select {|gh_contributor| fetch_contributor_email(gh_contributor["login"]) == contributor.email }
 					contributor.create_github_user(
-						username: gh_info.first["login"], 
-						gh_avatar_url: gh_info.first["avatar_url"], 
+						username: gh_info.first["login"],
+						gh_avatar_url: gh_info.first["avatar_url"],
 						gh_repo_url: gh_info.first["url"]
 					)
 			end
-		end	
+		end
 
 		rows.map do |repo_file|
 			new_file =	RepositoryFile.create_repo_files(repo_file, @username, saved_repository)
@@ -35,7 +38,17 @@ class CodeReviewsController < ApplicationController
 			end
 		end
 		@repository = saved_repository
-		render 'repositories/show'
+		redirect_to repository_path(@repository)
 	end
+
+	private
+
+  def sort_column
+    RepositoryFile.column_names.include?(params[:sort]) ? params[:sort] : "commits"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@
 
   def sortable(column, title = nil)
     title ||= column.titleize
-    direction =  column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    direction =  column == sort_column && sort_direction == "desc" ? "asc" : "desc"
     link_to title, sort: column, direction: direction
   end
 
